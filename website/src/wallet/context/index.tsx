bcd0f2c815f0dc63c9b42eb75d13e05a5f84c548
andy <andy@hippo.space>
2024-04-07 14:43:00 +0800

    Add key to Web3ReactProvider

--- website/src/wallet/context/index.tsx
+++ website/src/wallet/context/index.tsx
@@ -13,7 +13,7 @@ import { EVM_CURRENT_CONNECTOR_ID } from '../const';
 import { WalletconnectV2Connector } from '../evmConnector';
 import { EIP6963Connector, parseChainId } from '../evmConnector/eip6963';
 import { useInjectedProviderDetails } from '../evmConnector/eip6963/providers';
-// import type { EIP6963ProviderDetail } from '../evmConnector/eip6963/types';
+import type { EIP6963ProviderDetail } from '../evmConnector/eip6963/types';
 import { useConnector, useETHProvider, useModalStateValue } from '../hooks';
 import type { EvmConnector, Metadata, Wallet } from '../types/types';
 import { WalletType } from '../types/types';
@@ -210,43 +210,42 @@ const EvmConnectProviderInner = ({
     </EvmConnectContext.Provider>
   );
 };
-//@typescript-eslint/no-unused-vars
-// function getSelectedInjectors(
-//   allEip6963Injectors: readonly EIP6963ProviderDetail[],
-//   maxSelectCount: number,
-//   selectEip6963?: EIP6963Wallet[]
-// ) {
-//   let selectedEip6963Injectors: EIP6963ProviderDetail[];
-//   if (selectEip6963 && selectEip6963.length > 0) {
-//     selectedEip6963Injectors = [];
-//     for (const eip6963Wallet of selectEip6963) {
-//       if (selectedEip6963Injectors.length >= maxSelectCount) {
-//         break;
-//       }
-//       for (const eip6963Injector of allEip6963Injectors) {
-//         if (selectedEip6963Injectors.length >= maxSelectCount) {
-//           break;
-//         }
-//         if (eip6963Wallet.toString() === eip6963Injector.info.name) {
-//           selectedEip6963Injectors.push(eip6963Injector);
-//         }
-//       }
-//     }
-//     if (selectedEip6963Injectors.length < maxSelectCount) {
-//       for (const eip6963Injector of allEip6963Injectors) {
-//         if (selectedEip6963Injectors.length >= maxSelectCount) {
-//           break;
-//         }
-//         if (!selectedEip6963Injectors.includes(eip6963Injector)) {
-//           selectedEip6963Injectors.push(eip6963Injector);
-//         }
-//       }
-//     }
-//   } else {
-//     selectedEip6963Injectors = allEip6963Injectors.slice(0, maxSelectCount);
-//   }
-//   return selectedEip6963Injectors;
-// }
+function getSelectedInjectors(
+  allEip6963Injectors: readonly EIP6963ProviderDetail[],
+  maxSelectCount: number,
+  selectEip6963?: EIP6963Wallet[]
+) {
+  let selectedEip6963Injectors: EIP6963ProviderDetail[];
+  if (selectEip6963 && selectEip6963.length > 0) {
+    selectedEip6963Injectors = [];
+    for (const eip6963Wallet of selectEip6963) {
+      if (selectedEip6963Injectors.length >= maxSelectCount) {
+        break;
+      }
+      for (const eip6963Injector of allEip6963Injectors) {
+        if (selectedEip6963Injectors.length >= maxSelectCount) {
+          break;
+        }
+        if (eip6963Wallet.toString() === eip6963Injector.info.name) {
+          selectedEip6963Injectors.push(eip6963Injector);
+        }
+      }
+    }
+    if (selectedEip6963Injectors.length < maxSelectCount) {
+      for (const eip6963Injector of allEip6963Injectors) {
+        if (selectedEip6963Injectors.length >= maxSelectCount) {
+          break;
+        }
+        if (!selectedEip6963Injectors.includes(eip6963Injector)) {
+          selectedEip6963Injectors.push(eip6963Injector);
+        }
+      }
+    }
+  } else {
+    selectedEip6963Injectors = allEip6963Injectors.slice(0, maxSelectCount);
+  }
+  return selectedEip6963Injectors;
+}
 export const EvmConnectProvider = ({
   children,
   options,
@@ -267,18 +266,29 @@ export const EvmConnectProvider = ({
   console.log('rebuild EvmConnectProvider');
   const allEip6963Injectors = useInjectedProviderDetails();
   const allEvmConnectors = useMemo(() => {
-    console.log('rebuild Web3ReactProvider');
-    return allEip6963Injectors.map((providerDetail): [EvmConnector, Web3ReactHooks] => {
-      const [connector, hooks] = initializeConnector<EIP6963Connector>((actions) => {
-        return new EIP6963Connector({ actions, providerDetail });
-      });
-      return [connector as EvmConnector, hooks];
-    });
+    const selectedEip6963Injectors = getSelectedInjectors(
+      allEip6963Injectors,
+      evmConnectorMaxCount - 1,
+      evmSelectEip6963
+    );
+
+    const eip6963Connectors = selectedEip6963Injectors.map(
+      (providerDetail): [EvmConnector, Web3ReactHooks] => {
+        const [connector, hooks] = initializeConnector<EIP6963Connector>((actions) => {
+          return new EIP6963Connector({ actions, providerDetail });
+        });
+        return [connector as EvmConnector, hooks];
+      }
+    );
+    return [
+      ...eip6963Connectors,
+      ...evmConnectors.slice(0, evmConnectorMaxCount - eip6963Connectors.length)
+    ];
   }, [evmConnectors, allEip6963Injectors, evmConnectorMaxCount, evmSelectEip6963]);
 
   return (
     <BtcConnectProvider options={options} connectors={btcConnectors} autoConnect={autoConnect}>
-      <Web3ReactProvider connectors={allEvmConnectors}>
+      <Web3ReactProvider key={new Date().toISOString()} connectors={allEvmConnectors}>
         <EvmConnectProviderInner evmConnectors={allEvmConnectors} autoConnect={autoConnect}>
           {children}
         </EvmConnectProviderInner>

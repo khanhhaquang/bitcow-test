81a5b5fd6b7496209e061c749fe808931ff79ad4
Vu Phan <vuphan365@gmail.com>
2024-06-18 13:16:03 +0700

    style: 💄 Add bitlayer mask

--- website/src/pages/Campaign/components/MiningGala.tsx
+++ website/src/pages/Campaign/components/MiningGala.tsx
@@ -1,14 +1,15 @@
 import { Image } from 'antd';
 
 import bitlayerLogo from 'resources/img/bitlayerLogo.webp';
+import bitlayerMask from 'resources/img/bitlayerMask.webp';
 import imageYourRecord from 'resources/img/yourRecord.webp';
 
 import TaskRecord from './TaskRecord';
 
 const MiningGala = () => {
   return (
-    <div className="mt-11 flex h-fit max-w-[1200px] flex-col text-bc-white shadow-bc-swap backdrop-blur-lg tablet:mt-4">
-      <div className="grid h-[70px] grid-cols-2 items-center bg-[#FF8D00] px-6 py-3 text-black">
+    <div className="relative mt-11 flex h-fit max-w-[1200px] flex-col overflow-hidden text-bc-white shadow-bc-swap backdrop-blur-lg tablet:mt-4">
+      <div className="z-10 grid h-[70px] grid-cols-2 items-center bg-[#FF8D00] px-6 py-3 text-black">
         <div className="font-micro text-[36px]">BITLAYER MINING GALA</div>
         <div className="justify-self-end">
           <Image src={bitlayerLogo} width={40} height={37} preview={false} />
@@ -42,6 +43,9 @@ const MiningGala = () => {
         <TaskRecord
           record={{ taskid: '001', title: 'Rewards', progress: 100, finished: true }}
           tooltipContent="We are calculating the designated token rewards for you. Exact amount revealed soon."></TaskRecord>
+      </div>
+      <div className="absolute -right-3 top-0 z-0 h-auto w-[373px] opacity-50">
+        <Image src={bitlayerMask} preview={false} />
       </div>
     </div>
   );

36e7c7d4314d96bcf5f0a37d92fb125b794addbd
Vu Phan <vuphan365@gmail.com>
2024-06-18 12:22:08 +0700

    feat: 🎸 Add NoCampaign UI

--- website/src/pages/Campaign/components/NoCampaign.tsx
+++ website/src/pages/Campaign/components/NoCampaign.tsx
@@ -1,4 +1,9 @@
 const NoCampaign = () => {
-  return <></>;
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center gap-y-6">
+      <img src="/images/pools-no-data.webp" alt="loading" width={108} height={99} />
+      <div className="font-micro text-4xl text-bc-white">NO CAMPAIGN ON THIS CHAIN YET</div>
+    </div>
+  );
 };
 export default NoCampaign;
--- website/src/pages/Campaign/index.tsx
+++ website/src/pages/Campaign/index.tsx
@@ -1,16 +1,29 @@
 // import useMerlinWallet from 'hooks/useMerlinWallet';
 // import { useMemo } from 'react';
+import { useMemo } from 'react';
+import { CONFIG } from 'sdk';
+
+import useNetwork from 'hooks/useNetwork';
+
 import MiningGala from './components/MiningGala';
+import NoCampaign from './components/NoCampaign';
 
 const Campaign = () => {
+  const { currentNetwork } = useNetwork();
   // const { isLoggedIn } = useMerlinWallet();
+
+  const isCampaignExist = useMemo(() => {
+    const isBitLayerMiningGala = currentNetwork?.chainConfig?.chainId === CONFIG.bitlayer.chainId;
+    return isBitLayerMiningGala;
+  }, [currentNetwork]);
+
   // const content = useMemo(() => {
   //   return MiningGala();
   // }, [isLoggedIn]);
 
   return (
     <div className="relative flex flex-col items-center pt-20">
-      <MiningGala></MiningGala>
+      {isCampaignExist ? <MiningGala /> : <NoCampaign />}
     </div>
   );
 };
